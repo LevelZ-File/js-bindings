3fe34d3d4ef5fb02d1dd538491897c8d7ab8a9e7
Gregory Mitchell <gamercoder215@gmail.com>
2024-04-09 01:55:32 +0000

    Add Import

---
+++ index.js
@@ -0,0 +1,9 @@
+export { default as Coordinate } from './src/coordinate.js'
+export { default as Coordinate2D } from './src/coordinate.js'
+export { default as Coordinate3D } from './src/coordinate.js'
+
+// Block
+export { default as Block } from './src/block.js'
+
+// LevelObject
+export { default as LevelObject } from './src/object.js'
