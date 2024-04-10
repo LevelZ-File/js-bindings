e93b5a23a36fce6d5ebb52007513f1c8bff16dfa
Gregory Mitchell <gamercoder215@gmail.com>
2024-04-10 01:07:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// Coordinates
 export { default as Coordinate } from './src/coordinate.js'
 export { default as Coordinate2D } from './src/coordinate.js'
 export { default as Coordinate3D } from './src/coordinate.js'
+export { default as Dimension } from './src/coordinate.js'
 
 // Block
 export { default as Block } from './src/block.js'
+export { default as LevelObject } from './src/block.js'
 
-// LevelObject
-export { default as LevelObject } from './src/object.js'
+// Level
+export { default as Level } from './src/level.js'
+export { default as Level2D } from './src/level.js'
+export { default as Level3D } from './src/level.js'
+export { default as Scroll } from './src/level.js'
+
+// LevelBuilder
+export { default as LevelBuilder } from './src/builder.js'
